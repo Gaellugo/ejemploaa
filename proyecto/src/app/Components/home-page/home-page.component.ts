da61b73281a1f499414951892155e360df168d47
AnarcoPunisher <anarcopunkangel@hotmail.com>
2022-11-17 20:05:42 -0800

    -update

---
+++ proyecto/src/app/Components/home-page/home-page.component.ts
@@ -0,0 +1,24 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(){
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems,{
+        indicators:true,
+        fullWidth:true
+      });
+    });
+  }
+
+}
--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { prueba } from './prueba/prueba.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    prueba,
-    HelloWorldComponent
+    FooterComponent,
+    HeaderComponent,
+    HomePageComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
